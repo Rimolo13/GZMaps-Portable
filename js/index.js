3c51d9f2fa4766731db61673bf186d721b99bf55
Rimolo13 <dornichmaximo@gmail.com>
2026-05-22 08:06:11 +0200

    temp save

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const app = document.getElementById("app");
 async function load() {
   app.innerHTML = await (await fetch("/html/app.html")).text();
-  import("/js/script.js")
+  import("/js/script.js");
 }
 load();
+//(document.getElementById("app")).getAttribute("name")
--- online-src/js/index.js
+++ online-src/js/index.js
@@ -1,6 +1,10 @@
 const app = document.getElementById("app");
 async function load() {
-  app.innerHTML = await (await fetch("https://cdn.jsdelivr.net/gh/Rimolo13/GZMaps-Portable@main/html/app.html")).text();
-  import("https://cdn.jsdelivr.net/gh/Rimolo13/GZMaps-Portable@main/js/script.js")
+  app.innerHTML = await (
+    await fetch(
+      "https://cdn.jsdelivr.net/gh/Rimolo13/GZMaps-Portable@main/html/app.html",
+    )
+  ).text();
+  import("https://cdn.jsdelivr.net/gh/Rimolo13/GZMaps-Portable@main/js/script.js");
 }
 load();
