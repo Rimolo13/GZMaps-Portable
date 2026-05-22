3ede42bc25d06e69410cb6e510797f2d6f6de28e
Rimolo13 <dornichmaximo@gmail.com>
2026-05-22 10:43:55 +0200

    fixed some problems with caching

--- js/index.js
+++ js/index.js
@@ -1,7 +1,29 @@
+const version = "1.01";
 const app = document.getElementById("app");
+const app_version = document.getElementById("app").getAttribute("name");
+const link_list = [
+  "/favicon.svg",
+  "/css/index.css",
+  "/html/app.html",
+  "/js/script.js",
+];
+
+function linkmaker(listlinknumber) {
+  if (app_version === "normal") {
+    return link_list[listlinknumber] + `?v=${version}`;
+  } else {
+    return (
+      "https://cdn.jsdelivr.net/gh/Rimolo13/GZMaps-Portable@main" +
+      link_list[listlinknumber] +
+      `?v=${version}`
+    );
+  }
+}
+
 async function load() {
-  app.innerHTML = await (await fetch("/html/app.html")).text();
-  import("/js/script.js");
+  app.innerHTML = await (await fetch(linkmaker(2))).text();
+  document.getElementById("icon").href = linkmaker(0);
+  document.getElementById("css").href = linkmaker(1);
+  import(linkmaker(3));
 }
 load();
-//(document.getElementById("app")).getAttribute("name")
