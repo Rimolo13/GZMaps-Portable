e3d5a90798d5ed9d55b30ea0a72039b8ff60bf4e
Rimolo13 <dornichmaximo@gmail.com>
2026-06-08 08:19:54 +0200

    font a little bit bigger and fix a scaling problem

--- js/script.js
+++ js/script.js
@@ -4,11 +4,12 @@ const height_70p = (window.innerHeight / 100) * 70;
 
 correct_resize();
 
-window.addEventListener("resize", (e) => {
-  correct_resize();
+window.addEventListener("resize", () => {
+  setTimeout(correct_resize, 150)
 });
 
 function correct_resize() {
+  console.log("test")
   if (window.innerWidth < height_70p) {
     full_map.classList.add("full-map-scale-width");
     full_map.classList.remove("full-map-scale-hight");
